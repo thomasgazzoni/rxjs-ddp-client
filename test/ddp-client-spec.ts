636e6bdcda78c34ee1113500c0423b02883e1c65
Thomas Gazzoni <thomas.gazzoni@bridqe.com>
2016-12-07 23:46:14 +0800

    test(fix) ddp-client test

--- test/ddp-client-spec.ts
+++ test/ddp-client-spec.ts
@@ -6,16 +6,6 @@ describe('DDPClient', () => {
 
     describe('after onConnected()', () => {
 
-        describe('on receiving {msg: "connected"}', () => {
-            it('should set session id in keystore', () => {
-
-            });
-        });
-
-        it('should send a ping to the server', (done) => {
-            // let ddpClient = new DDPClient();
-            // ddpClient.ping();
-        });
 
     });
 });
